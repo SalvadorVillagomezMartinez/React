9c1b90836e72b8e816afc158af27f61bfa34f0ac
SalvadorVillagomezMartinez <110685719+SalvadorVillagomezMartinez@users.noreply.github.com>
2023-09-26 17:12:03 -0600

    GitExpertApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDom from 'react-dom/client';
+import {GitExpertApp} from "./GigExpertApp"
 import './styles.css'
-import { FirstApp } from "./FirstApp";
-import { CounterApp } from "./Counter-app";
-FirstApp
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp name="Max Verstappen"
-            message="you are the world champion"
-            title={2}
-            team="RedBull Racing" />
-        <CounterApp value={5}/>
+    
+        <GitExpertApp>
+        </GitExpertApp>
     
     </React.StrictMode>
 )
